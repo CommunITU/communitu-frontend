4460ed87e535a2b86df2aca4fda5d93ed09946a5
umutbayramoglu <umut265@gmail.com>
2020-11-14 01:13:00 +0300

    Create login-register tab.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,47 +2,68 @@ import React, {Component} from 'react';
 import NavbarNotif from "./NavbarNotif";
 import NavbarMessages from "./NavbarMessages";
 import {Navbar, Nav, NavItem, NavLink, Col, Row} from "react-bootstrap";
+import {FaSignInAlt, FaRegUser} from "react-icons/fa"
+import Container from "react-bootstrap/Container";
 
 class _Navbar extends Component {
 
     render() {
-        const isLoggedIn = true;
+        const isLoggedIn = false;
 
         return (
-            <Navbar expand="sm" collapseOnSelect variant="dark"  className="custom-header">
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav>
-                        <NavItem className="px-3">
-                            <NavLink href="/dashboard">Dashboard</NavLink>
-                        </NavItem>
-                        <NavItem className="px-3">
-                            <NavLink href="/users">Users</NavLink>
-                        </NavItem>
-                        <NavItem className="px-3">
-                            <NavLink href="/dashboard">Settings</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-
-
-                {
-                    isLoggedIn
-                        ? <Nav className="px-3 float-right">
-                           <Col>
-                               <Row>
-                                   <NavbarNotif/>
-                                   {/*<TheHeaderDropdownTasks/>*/}
-                                   <NavbarMessages/>
-                                   {/*<TheHeaderDropdown/>*/}
-                               </Row>
-                           </Col>
-
+            <Navbar expand="sm" collapseOnSelect variant="dark" className="custom-header align-items-stretch p-0">
+                <Container className="align-items-stretch">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav p-0">
+                        <Nav className="">
+                            <NavItem className="px-3 py-2 active">
+                                <NavLink class="" href="/dashboard">Dashboard</NavLink>
+                            </NavItem>
+                            <NavItem className="px-3 py-2">
+                                <NavLink href="/users">Users</NavLink>
+                            </NavItem>
+                            <NavItem className="px-3 py-2">
+                                <NavLink href="/dashboard">Settings</NavLink>
+                            </NavItem>
                         </Nav>
-                        : <div>No Login</div>
-                }
+                    </Navbar.Collapse>
 
 
+                    {
+                        isLoggedIn
+                            ? <Nav className="float-right mx-2">
+                                <Col class="col-12">
+                                    <Row>
+                                        <NavItem className="py-2">
+                                            <NavbarNotif/>
+                                        </NavItem>
+                                        <NavItem className="py-2">
+                                            <NavbarMessages/>
+                                        </NavItem>
+                                    </Row>
+                                </Col>
+                            </Nav>
+
+                            : <Nav navbar className="mx-2">
+                                <Col>
+                                    <Row>
+                                        <NavItem className="py-2">
+                                            <NavLink href="/login"><span
+                                                className="mr-2 text-gray"><FaSignInAlt/> Login</span></NavLink>
+                                        </NavItem>
+                                        <NavItem className="py-2">
+                                            <NavLink href="/register"><span
+                                                className="text-gray"><FaRegUser
+                                                className="mr-1 mb-1"/>Register</span></NavLink>
+                                        </NavItem>
+                                    </Row>
+                                </Col>
+
+                            </Nav>
+                    }
+
+
+                </Container>
             </Navbar>
         );
     }
