b53064b7a572d898c8f37148f0021ecfdea87608
umutbayramoglu <umut265@gmail.com>
2020-12-19 02:10:08 +0300

    Change api config.

--- src/util/constants/ApiConfig.js
+++ src/util/constants/ApiConfig.js
@@ -1,6 +1,6 @@
 /** Contains the api constants.  */
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://communitu-api.herokuapp.com";
 export const LOGIN = "/auth/login";
 export const LOGIN_WITH_TOKEN = "/auth/login_with_token";
 
