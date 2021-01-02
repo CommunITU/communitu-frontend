de7e09caa971fd2f8fd7d46d2fd359acda623dfe
umutbayramoglu <umut265@gmail.com>
2021-01-02 14:37:07 +0300

    Change api base url.

--- src/constants/ApiConfig.js
+++ src/constants/ApiConfig.js
@@ -1,6 +1,6 @@
 /** Contains the api constants.  */
 
-export const BASE_URL = "http://127.0.0.1:5000";
+export const BASE_URL = process.env.API_BASE_URL || "http://127.0.0.1:5000";
 export const LOGIN = "/auth/login";
 export const REGISTER = "/auth/register";
 export const LOGIN_WITH_TOKEN = "/auth/login_with_token";
